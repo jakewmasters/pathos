6aa9766d49d2cceeb926ec9ff6ce3b8cd938aa0a
jakewmasters <jakewmasters@gmail.com>
2019-06-20 13:13:17 -0500

    basic stage 0 in place, no loading yet

--- src/boot0.asm
+++ src/boot0.asm
@@ -5,11 +5,32 @@
 
 bits 16
 
-org 0x7c00
+org 0
 
 start:
-    jmp sign
+    jmp main
 
-sign:
+load:
+    nop
+    ret
+
+main:
+    cli
+    ; adjust segment registers
+    mov    ax, 0x07c0
+    mov    ds, ax        ; data segment
+    mov    es, ax        ; extra segment
+    mov    fs, ax        ; general-purpose
+    mov    gs, ax        ; general-purpose
+    ; create stack
+    mov    ax, 0x0000
+    mov    ss, ax
+    mov    sp, 0xFFFF
+    sti
+
+    ; do all of our loading here
+    call load
+    ; treat load as a black box, it should just work
+
     times 510-($-$$) db 0
     dw 0xaa55
