b64c49e0e0d9f6074e426e693ec397aa254407ee
Jake Masters <jakewmasters@gmail.com>
2019-07-12 13:51:33 -0500

    cleaned up bootloader, add comments

--- bootloader/bootloader.s
+++ bootloader/bootloader.s
@@ -11,33 +11,37 @@ start:
     mov ss, ax
 
     ; setup stack
-    mov bp, 0x8000 ; 1 kb past start of code
+    mov bp, 0x8c00 ; 4 kb past start of code
     mov sp, bp
 
     ; load 5 sectors from boot disk
-    mov bx, 0x9000
-    mov dh, 5
-    mov dl, [BOOT_DRIVE]
+    mov bx, 0x9000        ; bx = buffer
+    mov dh, 5             ; 5 sectors
+    mov dl, [BOOT_DRIVE]  ; drive set to BOOT_DRIVE
     call disk_load
 
     ; check to see if sectors loaded correctly
-    mov dx, [0x9000]
+    mov dx, [0x9000] ; 2 bytes of hex data stored in dx
     call print_hex
 
+    ; test second print_hex
+    mov dx, 0x1234
+    call print_hex
 
 hang:
     jmp hang
 
 
-; extra functions
+;; extra functions ;;
+
 disk_load:
     push dx
-    mov ah, 0x02
-    mov al, dh
-    mov ch, 0x0
-    mov dh, 0x0
-    mov cl, 0x02
-    int 0x13
+    mov ah, 0x02  ; set disk read mode in BIOS
+    mov al, dh    ; number of sectors to read
+    mov ch, 0x0   ; cylinder number
+    mov dh, 0x0   ; head
+    mov cl, 0x02  ; sector
+    int 0x13      ; call interrupt to read!
     jc disk_error
     pop dx
     cmp dh, al
@@ -53,46 +57,44 @@ print_string:
     pusha
     mov ah, 0x0e
 loop:
-    mov al, [bx]
+    mov al, [bx] ; bx will have string to print
     int 0x10
     add bx, 1
-    cmp al, 0
+    cmp al, 0    ; stops when null byte found
     jne loop
     popa
     ret
 
+; WARNING: does not clear hex string, must set dx every time
 print_hex:
     mov cx, dx
-    and cx, 0xf000
-    shr cx, 12
+    and cx, 0xf000         ; grab most significant byte
+    shr cx, 12             ; shift to cl
     call to_char
-    mov [HEX_OUT + 2], cx
+    mov [HEX_OUT + 2], cx  ; place char in hex string after 0x prefix
 
     mov cx, dx
-    and cx, 0x0f00
+    and cx, 0x0f00         ; repeat for second most significant byte
     shr cx, 8
     call to_char
     mov [HEX_OUT + 3], cx
 
     mov cx, dx
-    and cx, 0x00f0
+    and cx, 0x00f0         ; repeat for third most significant byte
     shr cx, 4
     call to_char
     mov [HEX_OUT + 4], cx
 
     mov cx, dx
-    and cx, 0x000f
+    and cx, 0x000f         ; repeat for least significant byte
     call to_char
     mov [HEX_OUT + 5], cx
 
     mov bx, HEX_OUT
     call print_string
-    mov byte [HEX_OUT + 2], '0'
-    mov byte [HEX_OUT + 3], '0'
-    mov byte [HEX_OUT + 4], '0'
-    mov byte [HEX_OUT + 5], '0'
     ret
 
+; stores appropriate char in cx
 to_char:
     cmp cx, 0xa
     jl digits
@@ -105,7 +107,7 @@ digits:
 
 ; bootloader variables
 BOOT_DRIVE: db 0
-DISK_ERROR_MSG db "Disk read error!", 0
+DISK_ERROR_MSG db "Could not read disk...", 0
 HEX_OUT: db '0x0000', 0
 
 ; sign boot sector
